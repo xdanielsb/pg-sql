57492f9bd747584bf28a1eadd4691ba3f4a7e8e7
Daniel <dfsantosbu@unal.edu.co>
2021-01-13 10:09:04 +0100

    chore: add exception e.g.

---
+++ exception.sql
@@ -0,0 +1,24 @@
+do
+$$
+declare 
+  rec record;
+  std_id int:=10;
+begin 
+  -- select student
+  select nombre1, apellido1
+  into strict rec
+  from estudiante 
+  where id_estudiante = std_id;
+
+  -- catch exception
+  exception
+    when no_data_found then
+      raise exception 'there is no students with id=%', std_id;
+end;
+$$;
+
+/*
+You have other options of exception for instance
+when no_data_found --> no data found
+when too_many_rows --> many rows found it
+*/
